54c8d712a133641ed38759e61a0ea6e92dd93f9d
Launer738 <katavin2025@gmail.com>
2026-02-09 19:49:12 +0300

    Классы

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+class Task():
+    tasks = []
+
+    def __init__(self, opis, srok, status=False):
+        self.opis = opis
+        self.srok = srok
+        self.status = status
+        Task.tasks.append(self)
+
+    def add_task(self, opis, srok):
+        Task(opis, srok)
+
+    def mark_completed(self):
+        self.status = True
+
+    def show_current_tasks(self):
+        for task in Task.tasks:
+            if task.status == False:
+                print(f"Описание: {task.opis}, Срок: {task.srok}")
+
+
+manager = Task("", "")
+manager.add_task("Купить молоко", "2024-01-15")
+manager.add_task("Сделать уроки", "2024-01-16")
+manager.add_task("Позвонить маме", "2024-01-17")
+
+Task.tasks[1].mark_completed()
+manager.show_current_tasks()
---
+++ main2.py
@@ -0,0 +1,39 @@
+class Warrior():
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+
+    def sleep(self):
+        print(f"{self.name} лег спать")
+        self.endurance += 2
+
+    def eat(self):
+        print(f"{self.name} сел кушать")
+        self.power += 1
+
+    def hit(self):
+        print(f"{self.name} бьет кого-то")
+        self.endurance -= 6
+
+    def walk(self):
+        print(f"{self.name} гуляет")
+
+    def info(self):
+        print(f"имя воина - {self.name}")
+        print(f"цвет волос воина - {self.hair_color}")
+        print(f"сила воина - {self.power}")
+        print(f"выносливость воина - {self.endurance}")
+
+war1 = Warrior(name="Степа", power=76, endurance=54, hair_color="коричневый")
+print(war1)
+
+print(war1.name)
+print(war1.power)
+print(war1.endurance)
+print(war1.hair_color)
+
+print(war1.endurance)
+war1.sleep()
+print(war1.endurance)
