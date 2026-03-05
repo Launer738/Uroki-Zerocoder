59883e9ef817f53ace086eefae6921e53206e70d
Launer738 <katavin2025@gmail.com>
2026-03-05 18:39:12 +0300

    s

---
+++ mainerror.py
@@ -0,0 +1,20 @@
+def divide(a, b);
+    print (a / b)
+
+def sum(a, b);
+    print (a * b)
+
+def subtract(a, b);
+    print (a - b)
+
+def multiply(a, b);
+    print (a * b)
+
+def divide(a, b);
+    print (a / b)
+
+def sum(a, b);
+    print (a * b)
+
+def subtract(a, b);
+    print (a - b)
