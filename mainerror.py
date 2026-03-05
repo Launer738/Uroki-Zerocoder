5b3fe1721482ef5bd8262f94e8971ecfcb4d20dd
Launer738 <katavin2025@gmail.com>
2026-03-05 18:41:16 +0300

    Ветка Егора

--- mainerror.py
+++ mainerror.py
@@ -1,5 +1,17 @@
 def divide(a, b);
-    print (a / b)
+    print (a / b);
+
+divide(10, 0);
 
 def sum(a, b);
-    print (a * b)
+    print (a + b);
+
+sum(10, 20);
+
+def subtract(a, b);
+    print (a - b);
+
+subtract(10, 20);
+
+def multiply(a, b);
+    print (a * b);
